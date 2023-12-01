f1f3d61420343d308519f1d5f9bbca617c481b9f
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2023-12-02 01:38:26 +0600

    Add files via upload

---
+++ homework/homework(one).php
@@ -0,0 +1,66 @@
+<?php
+   //string
+   echo 'i am harvey.<br>';
+   echo 'i am badhon. <br>';
+   echo 'i am badhon. <br>';
+   echo 'i am badhon. <br>';
+    $name = 'pabel';
+   echo '$name';
+   
+
+   echo "myself badhon.";
+   
+
+  echo  "hi universe.<br>";
+   
+  echo 'how are you?';
+
+  echo  'Myself Badhan, I am 23 years old'; 
+
+  echo "576.90";
+
+
+    //number
+  echo 567.90;
+  
+
+   //boolean
+
+  echo true;
+
+
+  echo false;
+
+ 
+
+
+ //variable
+ $name = "Harvehy";
+echo $name;
+
+ echo 'i am $name. <br>';
+//concatenation
+$name = "Harvehy";
+echo 'I am'  .$name. "I am 25 years old";
+  $name = "onie";
+  $age = 25;
+  $hobby = "coding";
+  echo 'I am'  . $name . '.I am' . $age . 'years old. I am' .$hobby. 'to make websites';
+
+  $name = "onie";
+  $age = 25;
+  $hobby = "coding";
+  echo "Myself $name. I am $age years old. I am $hobby to make websites.";
+     echo "<p>Myself {$name}.</p>";
+   echo '$name';
+  $name = "mizan";
+  echo $name;
+  $name = "onie";
+
+
+  //constant
+  define('pi', 3.1416);
+   echo pi;
+
+?> 
+   
